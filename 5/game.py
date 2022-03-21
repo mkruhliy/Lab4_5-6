595168caadf92c9cddf30a13547fd97f701ca0f6
Markiyan Kruhliy <mkruhliy@gmail.com>
2022-03-21 12:34:25 +0200

    minor changesv3

--- 5/game.py
+++ 5/game.py
@@ -3,27 +3,34 @@ class Info(object):
     Helping class, indicates the number
     of defeated enemies
     """
+
     defeated = 0
 
 info = Info()
 
+
 class Room:
     """Room that keeps items and characters"""
+    
     def __init__(self, room) -> None:
         self.room = room
         self.dscrpt = ''
         self.link = {}
         self.character = None
         self.item = None
+    
     def set_description(self, statment):
         """setting description of the room"""
         self.dscrpt = statment
+    
     def link_room(self, room, side):
         """setting connected room"""
         self.link[side] = room
+    
     def move(self, side):
         """changing room"""
         return self.link[side]
+    
     def get_details(self):
         """representation of the room"""
         print(self.room)
@@ -31,45 +38,58 @@ class Room:
         print(self.dscrpt)
         for key, value in self.link.items():
             print("The {} is {}".format(value.room, key))
+    
     def set_character(self, character):
         """setting character in the room"""
         self.character = character
+    
     def get_character(self):
         """returning character of the room"""
         return self.character
+    
     def set_item(self, item):
         """setting item in the room"""
         self.item = item
+    
     def get_item(self):
         """returning item of the room"""
         return self.item
 
+
 class Character:
     """Enemy or Friend"""
+    
     def __init__(self, name, descr) -> None:
         self.name = name
         self.descr = descr
         self.conversation = None
+    
     def set_conversation(self, message):
         """setting character's conversation message"""
         self.conversation = message
+    
     def talk(self):
         """returning character's monologue"""
         print("[{} says]: {}".format(self.name, self.conversation))
 
+
 class Enemy(Character):
     """Character type"""
+    
     def __init__(self, name, descr) -> None:
         super().__init__(name, descr)
         self.conversation = None
         self.weakness = None
+    
     def set_weakness(self, weak):
         """setting item that could overcome the enemy """
         self.weakness = weak
+    
     def describe(self):
         """short info about enemy existance in the room"""
         print("{} is here".format(self.name))
         print(self.descr)
+    
     def fight(self, fight_with):
         """fighting enemy with particular item"""
         if self.weakness == fight_with:
@@ -79,15 +99,19 @@ class Enemy(Character):
         else:
             print("{} crushes you, puny adventurer!".format(self.name))
             return False
+    
     def get_defeated(self):
         """returning the number of enemies defeted"""
         return info.defeated
 
+
 class Friend(Character):
     """Character type"""
+    
     def __init__(self, name, descr) -> None:
         super().__init__(name, descr)
         self.conversation = None
+    
     def describe(self):
         """Short info about the friend"""
         print("{} (your friend) is here".format(self.name))
@@ -96,15 +120,19 @@ class Friend(Character):
 
 class Item:
     """Object used for a fight"""
+    
     def __init__(self, item) -> None:
         self.item = item
         self.description = None
+    
     def set_description(self, statment):
         """setting description of the item"""
         self.description = statment
+    
     def describe(self):
         """short maessage about item existance in the room"""
         print("The [{}] is here - {}".format(self.item, self.description))
+    
     def get_name(self):
         """returning name of the item"""
         return self.item
--- 6/game_custom.py
+++ 6/game_custom.py
@@ -1,31 +1,40 @@
 class City:
     """place for main action"""
+
     def __init__(self, name=None) -> None:
         self.name = name
         self.character = None
         self.item = None
         self.link = None
+    
     def set_character(self, character):
         """setting character in the city"""
         self.character = character
+    
     def set_item(self, item):
         """setting item in the city"""
         self.item = item
+    
     def get_details(self):
         """getting city name"""
         print("\n                 "+self.name)
+    
     def link_city(self, city):
         """setting connecting cities"""
         self.link = city
+    
     def get_character(self):
         """getting city character"""
         return self.character
+    
     def get_item(self):
         """getting city item"""
         return self.item
+    
     def get_link(self):
         """getting cities connected with the current"""
         return self.link
+    
     def empty_city(self):
         """presentation of the empty city"""
         print('      O \n\
@@ -35,16 +44,20 @@ _____/_\____________________________________')
 
 class Character:
     """characters and their general behaviour"""
+    
     def __init__(self, name=None, hp=None) -> None:
         self.name = name
         self.hp = hp
         self.item = None
+    
     def get_hp(self):
         """getting character health points"""
         return self.hp
+    
     def set_gun(self, gun):
         """setting a gun for the character"""
         self.item = gun
+    
     def get_gun(self):
         """getting character's gun"""
         return self.item
@@ -52,26 +65,33 @@ class Character:
 
 class MainCharacter(Character):
     """main character behaviour"""
+    
     def __init__(self, name=None, hp=None, description=None) -> None:
         super().__init__(name, hp)
         self.description = description
         self.item = None
         self.backpack = []
+    
     def backpack_capacity(self):
         """getting backpack items"""
         return self.backpack
+    
     def append_backpack(self, elem):
         """adding items to the backpack"""
         self.backpack.append(elem)
+    
     def backpack_repr(self):
         """getting backpack items but in the pretty-looking form"""
         return [x.name for x in self.backpack]
+    
     def get_description(self):
         """getting description of the main character"""
         print("Your soldier: {}\nHP: {}\n'{}'".format(self.name, self.hp, self.description))
+    
     def heal(self, obj):
         """adding haelth points"""
         self.hp += obj.dm
+    
     def fight(self, chr):
         """commiting a battle"""
         self.hp -= chr.item.dm
@@ -93,15 +113,18 @@ class MainCharacter(Character):
 
 class Enemy(Character):
     """enemy behaviour"""
+    
     def __init__(self, name=None, hp=None, tp=None) -> None:
         super().__init__(name, hp)
         self.item = None
         self.type = tp
+    
     def get_description(self):
         """getting description of the enemy"""
         print("      O_,~-                     -~,_O\n\
      /|                             |\n\
 _____/_\___________________________/_\______\nYour opponent: {} -  hp:{}\n(type 'fight' to attack)".format(self.name, self.hp))
+    
     def get_type(self):
         """getting if 'simple' or 'super' enemy"""
         return self.type
@@ -109,9 +132,11 @@ _____/_\___________________________/_\______\nYour opponent: {} -  hp:{}\n(type 
 
 class SuperEnemy(Enemy):
     """'super' enemy behaviour"""
+    
     def  __init__(self, name=None, hp=None, tp=None) -> None:
         super().__init__(name, hp, tp)
         self.item = None
+    
     def get_description(self):
         """getting description of the 'super' enemy"""
         print("	                      __/___\n\
@@ -119,6 +144,7 @@ class SuperEnemy(Enemy):
      /|         _______/_____\_______\_____ \n\
 _____/_\_______ \              < < <       |\n\
 Your SUPER OPPONENT: {}  -  hp:{}\n(type 'fight' to attack)".format(self.name, self.hp))
+    
     def final(self):
         """final exam to be passed for a win"""
         first = input("\nBut to win the russian regime answer those questions.\n\
@@ -136,12 +162,15 @@ Your final exam:\n'Ruskiy korabl idi ... '?:\n1) nahuy\n2) domoy\n3) palanitsa\n
 
 class Item:
     """item general behaviour"""
+    
     def __init__(self, name=None, dm=None) -> None:
         self.name = name
         self.dm = dm
+    
     def get_name(self):
         """getting item's name"""
         return self.name
+    
     def get_dm(self):
         """getting item's damage points"""
         return self.dm
@@ -149,8 +178,10 @@ class Item:
 
 class Gun(Item):
     """gun behaviour"""
+    
     def __init__(self, name=None, dm=None) -> None:
         super().__init__(name, dm)
+    
     def get_description(self, obj):
         """getting description of the gun"""
         print('-'*18+'\n{} Gun: {} \ndamage: {}\n  ︻╦╤───\n'.format(obj, self.name, self.dm)+'-'*18)
@@ -158,8 +189,10 @@ class Gun(Item):
 
 class Support(Item):
     """medicaments behaviour"""
+    
     def __init__(self, name=None, dm=None) -> None:
         super().__init__(name, dm)
+    
     def get_description(self):
         """meds description"""
         print("      O                 _______  \n\
